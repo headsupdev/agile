d1ec10c2058853bbbebbfc91f2a8fe1ee8089c95
Andrew Williams <andy@andywilliams.me>
2012-06-16 21:07:46 +0100

    Update windows script so cmd-c works properly

--- agile-runtime/src/main/scripts/agile.bat
+++ agile-runtime/src/main/scripts/agile.bat
@@ -1,5 +1,13 @@
 @echo off
 
+:: Try to get around "Terminate batch job?" - use start, which spawns a new window
+:: Establish if we were launched from the command line.
+:: If so, run a start /wait otherwise we want the original window to close
+set SCRIPT=%0
+set DQUOTE="
+@echo %SCRIPT:~0,1% | findstr /l %DQUOTE% > NUL
+if NOT %ERRORLEVEL% EQU 0 set START_PARAMS=/wait
+
 set OLDDIR=%CD%
 cd %~dp0
 cd ..
@@ -19,7 +27,7 @@ set JAVA_OPTS=-Xmx512m -Djava.awt.headless=true -Duser.timezone=UTC
 
 cd %AGILE_HOME%
 
-%JAVA_EXE% %JAVA_OPTS% -cp conf;bin\${project.artifactId}-${project.version}.jar org.headsupdev.agile.runtime.Main %CMD_LINE_ARGS%
+@start %START_PARAMS% cmd /c %JAVA_EXE% %JAVA_OPTS% -cp conf;bin\${project.artifactId}-${project.version}.jar org.headsupdev.agile.runtime.Main %CMD_LINE_ARGS%
 
 set CMD_LINE_ARGS=
 set JAVA_EXE=
