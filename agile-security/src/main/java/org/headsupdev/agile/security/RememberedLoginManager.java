afc38165088ec5ac16c2062f90d1d3c6f64e0fa4
Andrew Williams <andy@andywilliams.me>
2013-02-03 17:15:33 +0000

    Allow many remember me cookies for the same account

--- agile-security/src/main/java/org/headsupdev/agile/security/RememberedLoginManager.java
+++ agile-security/src/main/java/org/headsupdev/agile/security/RememberedLoginManager.java
@@ -22,8 +22,7 @@ import org.headsupdev.agile.api.Manager;
 import org.headsupdev.agile.api.User;
 import org.headsupdev.support.java.Base64;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * A login manager class that handles remembering users on the system
@@ -37,7 +36,7 @@ public abstract class RememberedLoginManager
 {
     private static final String REMEMBER_STORE_KEY = "remembermes";
 
-    private static Map<String, String> rememberMap = null;
+    private static Map<String, Set<String>> rememberMap = null;
 
     public User getRememberedUser( String context )
     {
@@ -48,7 +47,8 @@ public abstract class RememberedLoginManager
             String username = context.substring( 0, pos );
             String key = context.substring( pos + 1 );
 
-            if ( key.equals( rememberMap.get( username ) ) ) {
+            Set<String> remembers = rememberMap.get( username );
+            if ( remembers != null && remembers.contains( key ) ) {
                 return Manager.getSecurityInstance().getUserByUsername( username );
             }
         }
@@ -59,7 +59,13 @@ public abstract class RememberedLoginManager
     public void rememberUser( User user, String random )
     {
         checkRemembers();
-        rememberMap.put( user.getUsername(), random );
+        Set<String> remembers = rememberMap.get(user.getUsername());
+        if ( remembers == null )
+        {
+            remembers = new HashSet<String>();
+        }
+        remembers.add( random );
+        rememberMap.put( user.getUsername(), remembers );
         saveRemembers();
     }
 
@@ -77,7 +83,7 @@ public abstract class RememberedLoginManager
             return;
         }
 
-        rememberMap = new HashMap<String,String>();
+        rememberMap = new HashMap<String,Set<String>>();
         String data = Manager.getStorageInstance().getGlobalConfiguration().getProperty( REMEMBER_STORE_KEY );
         if ( data == null ) {
             return;
@@ -93,7 +99,7 @@ public abstract class RememberedLoginManager
                 continue;
             }
 
-            rememberMap.put( parts[0], parts[1] );
+            rememberMap.put( parts[0], setFromString( parts[1] ) );
         }
     }
 
@@ -109,7 +115,7 @@ public abstract class RememberedLoginManager
         {
             buffer.append( user );
             buffer.append( ":" );
-            buffer.append( rememberMap.get( user ) );
+            buffer.append( stringFromSet( rememberMap.get( user ) ) );
             buffer.append( ";" );
         }
         String data = buffer.toString();
@@ -120,5 +126,37 @@ public abstract class RememberedLoginManager
 
         data = new String( Base64.encodeBase64( data.getBytes() ) );
         Manager.getStorageInstance().getGlobalConfiguration().setProperty( REMEMBER_STORE_KEY, data );
+    }
+
+    private String stringFromSet( Set<String> set )
+    {
+        StringBuilder builder = new StringBuilder();
+
+        if ( set == null || set.size() == 0 )
+        {
+            return "";
+        }
+
+        for ( String string : set )
+        {
+            builder.append( string );
+            builder.append( "," );
+        }
+
+        String ret = builder.toString();
+        return ret.substring( 0, ret.length() - 1 );
+    }
+
+    private Set<String> setFromString( String input )
+    {
+        Set<String> ret = new HashSet<String>();
+        if ( input == null || input.length() == 0 )
+        {
+            return ret;
+        }
+
+        String[] split = input.split( "," );
+        ret.addAll( Arrays.asList( split ) );
+        return ret;
     }
 }
