732ba4a42bc3e37f4f1c04436edeabe2831947d5
Andrew Williams <andy@andywilliams.me>
2013-04-22 00:43:39 +0100

    Correct copyright in footer

--- agile-web/src/main/java/org/headsupdev/agile/web/internal/WebManagerImpl.java
+++ agile-web/src/main/java/org/headsupdev/agile/web/internal/WebManagerImpl.java
@@ -67,7 +67,7 @@ public class WebManagerImpl
     @Override
     public String getFooterCopyrightHTML()
     {
-        return "Copyright &copy; 2009 - 2012 <a href=\"http://headsupdev.com/\" target=\"_blank\">" +
+        return "Copyright &copy; 2009 - 2013 <a href=\"http://headsupdev.com/\" target=\"_blank\">" +
             "Heads Up Development Ltd</a>.";
     }
 
