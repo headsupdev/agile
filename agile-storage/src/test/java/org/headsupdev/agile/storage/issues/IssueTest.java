2608a2cdb7e9c5ea896c8a1c1a177639feb8dd84
Gordon Edwards <gordon.edwards@kotikan.com>
2014-06-19 17:13:35 +0100

    Adding unit tests for issue relationships

---
+++ agile-storage/src/test/java/org/headsupdev/agile/storage/issues/IssueTest.java
@@ -0,0 +1,120 @@
+/*
+ * HeadsUp Agile
+ * Copyright 2014 Heads Up Development Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.headsupdev.agile.storage.issues;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import org.headsupdev.agile.storage.StoredFileProject;
+
+/**
+ * A simple milestone to gather info on target releases.
+ *
+ * @author gordonedwards
+ * @since 2.1
+ */
+public class IssueTest
+        extends TestCase
+{
+
+    private Issue issue1;
+    private Issue issue2;
+
+    public void setUp()
+            throws Exception
+    {
+        super.setUp();
+        StoredFileProject project1 = new StoredFileProject( "proj1", "proj1" );
+        StoredFileProject project2 = new StoredFileProject( "proj2", "proj2" );
+        issue1 = new Issue( project1 );
+        issue2 = new Issue( project2 );
+    }
+
+    public void testDetectDuplicateLinkedRelationships()
+    {
+        IssueRelationship relationship1 = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_LINKED );
+
+        Assert.assertFalse( issue1.hasRelationship( relationship1 ) );
+        issue1.getRelationships().add( relationship1 );
+        Assert.assertTrue( issue1.hasRelationship( relationship1 ) );
+    }
+
+    public void testEquivalenceLinkedRelationships()
+    {
+        IssueRelationship relationship1 = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_LINKED );
+        IssueRelationship relationship2 = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_LINKED );
+
+        Assert.assertTrue( relationship1.isEquivalent( relationship2 ) );
+    }
+
+    public void testInvertEquivalenceLinkedRelationships()
+    {
+        IssueRelationship relationship1 = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_LINKED );
+        IssueRelationship relationship2 = new IssueRelationship( issue2, issue1, IssueRelationship.TYPE_LINKED );
+
+        Assert.assertTrue( relationship1.isEquivalent( relationship2 ) );
+    }
+
+    public void testNonEquivalenceLinkedRelationships()
+    {
+        StoredFileProject project3 = new StoredFileProject( "proj3", "proj3" );
+        Issue issue3 = new Issue( project3 );
+
+        IssueRelationship relationship1 = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_LINKED );
+        IssueRelationship relationship2 = new IssueRelationship( issue3, issue1, IssueRelationship.TYPE_LINKED );
+
+        Assert.assertFalse( relationship1.isEquivalent( relationship2 ) );
+    }
+
+    public void testEquivalenceBlocksRelationships()
+    {
+        IssueRelationship relationship1 = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_BLOCKS );
+        IssueRelationship relationship2 = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_BLOCKS );
+
+        Assert.assertTrue( relationship1.isEquivalent( relationship2 ) );
+    }
+
+    public void testInvertEquivalenceBlockRelationships()
+    {
+        IssueRelationship relationship1 = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_BLOCKS );
+        IssueRelationship relationship2 = new IssueRelationship( issue2, issue1, IssueRelationship.TYPE_BLOCKS );
+
+        Assert.assertTrue( relationship1.isEquivalent( relationship2 ) );
+    }
+
+    public void testDoubleInvertedIsSameLinkedRelationships()
+    {
+        IssueRelationship relationship = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_LINKED );
+        IssueRelationship invertedRelationship = relationship.getInverseRelationship();
+        Assert.assertTrue( relationship.equals( invertedRelationship.getInverseRelationship() ) );
+    }
+
+    public void testDoubleInvertedIsSameBlocksRelationships()
+    {
+        IssueRelationship relationship = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_BLOCKS );
+        IssueRelationship invertedRelationship = relationship.getInverseRelationship();
+        Assert.assertTrue( relationship.equals( invertedRelationship.getInverseRelationship() ) );
+    }
+
+    public void testDoubleInvertedIsSameBlocksFromInvertedRelationships()
+    {
+        IssueRelationship invertedRelationship = new IssueRelationship( issue1, issue2, IssueRelationship.TYPE_BLOCKS + IssueRelationship.REVERSE_RELATIONSHIP );
+        IssueRelationship relationship = invertedRelationship.getInverseRelationship();
+        Assert.assertTrue( relationship.equals( invertedRelationship.getInverseRelationship() ) );
+    }
+}
