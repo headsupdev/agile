8c980d0f7cbf7de82a4f7a912dc25d2fe9b60e4c
Gordon Edwards <gordon.edwards@kotikan.com>
2014-08-11 11:39:16 +0100

    Adding copyright notice and description to OnePressButton

--- agile-web/src/main/java/org/headsupdev/agile/web/components/OnePressButton.java
+++ agile-web/src/main/java/org/headsupdev/agile/web/components/OnePressButton.java
@@ -1,3 +1,21 @@
+/*
+ * HeadsUp Agile
+ * Copyright 2014 Heads Up Development Ltd.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.headsupdev.agile.web.components;
 
 import org.apache.wicket.markup.html.form.Button;
@@ -5,6 +23,8 @@ import org.apache.wicket.model.IModel;
 
 /**
  * Created by Gordon Edwards on 06/08/2014.
+ *
+ * A button that disables itself when pressed
  */
 public class OnePressButton
         extends Button
