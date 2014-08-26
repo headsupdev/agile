b0d31349672ed4580a4344b433e1b92e4e64cbff
Gordon Edwards <gordon.edwards@kotikan.com>
2014-08-26 14:20:12 +0100

    Formatting changes

--- agile-apps/agile-app-admin/src/main/java/org/headsupdev/agile/app/admin/event/AccountAddEvent.java
+++ agile-apps/agile-app-admin/src/main/java/org/headsupdev/agile/app/admin/event/AccountAddEvent.java
@@ -1,6 +1,6 @@
 /*
  * HeadsUp Agile
- * Copyright 2009-2014 Heads Up Development Ltd.
+ * Copyright 2009-2012 Heads Up Development Ltd.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
@@ -19,7 +19,6 @@
 package org.headsupdev.agile.app.admin.event;
 
 import org.headsupdev.agile.app.admin.AdminApplication;
-import org.headsupdev.agile.web.HeadsUpPage;
 import org.headsupdev.agile.web.components.UserDetailsPanel;
 import org.headsupdev.agile.web.RenderUtil;
 import org.headsupdev.agile.web.AbstractEvent;
@@ -64,7 +63,7 @@ public class AccountAddEvent
         {
             public Panel getPanel()
             {
-                return new UserDetailsPanel( RenderUtil.PANEL_ID, user, project, false);
+                return new UserDetailsPanel( RenderUtil.PANEL_ID, user, project, false );
             }
         }.getRenderedContent();
     }
